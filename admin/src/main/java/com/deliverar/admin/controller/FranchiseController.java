a7e4e5955fd8c10ebe2200d5b3960d2876736007
CctsJck <julipro99@gmail.com>
2022-09-30 19:33:33 -0300

    Documentation for addresses and franchises

--- admin/src/main/java/com/deliverar/admin/controller/FranchiseController.java
+++ admin/src/main/java/com/deliverar/admin/controller/FranchiseController.java
@@ -1,10 +1,18 @@
 package com.deliverar.admin.controller;
 
 
+import com.deliverar.admin.model.dto.ExceptionResponse;
 import com.deliverar.admin.model.dto.Franchise.FranchiseRequest;
 import com.deliverar.admin.model.dto.Franchise.FranchiseResponse;
 import com.deliverar.admin.model.dto.Franchise.FranchiseUpdateRequest;
+import com.deliverar.admin.model.dto.Operator.OperatorResponse;
 import com.deliverar.admin.service.FranchiseService.FranchiseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,33 +22,46 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/franchise")
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Operator found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = FranchiseResponse.class))),
+        @ApiResponse(responseCode = "404", description = "Operator not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+}
+
+)
+@Tag(name = "Franchise API", description = "Franchise API developed by Deliverar Administrator")
 public class FranchiseController {
 
     @Autowired
     private FranchiseService franchiseService;
 
     @PostMapping("/")
+    @Operation(summary = "Create a new Franchise", description = "Create a new Franchise")
     public ResponseEntity<FranchiseResponse> save(@RequestBody FranchiseRequest franchiseRequest){
         return new ResponseEntity<>(franchiseService.saveNewFranchise(franchiseRequest), HttpStatus.OK);
     }
 
     @PutMapping("/")
+    @Operation(summary = "Update a Franchise", description = "Update a Franchise")
     public ResponseEntity<FranchiseResponse> update (@RequestBody FranchiseUpdateRequest franchiseUpdateRequest){
         return new ResponseEntity<>(franchiseService.update(franchiseUpdateRequest),HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Franchise",description = "Delete a Franchise")
     public ResponseEntity deleteById(@PathVariable Long id){
         franchiseService.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find a Franchise by ID",description = "Find a Franchise by ID")
     public ResponseEntity<FranchiseResponse> getFranchiseById(@PathVariable Long id){
         return new ResponseEntity<>(franchiseService.getFranchiseResponseById(id),HttpStatus.OK);
     }
 
     @GetMapping("/")
+    @Operation(summary = "Find all Franchises",description = "Find all Franchises")
     public ResponseEntity<List<FranchiseResponse>> getAllFranchises(){
         return new ResponseEntity<>(franchiseService.getAllFranchises(),HttpStatus.OK);
     }
--- admin/src/main/java/com/deliverar/admin/model/dto/Franchise/FranchiseUpdateRequest.java
+++ admin/src/main/java/com/deliverar/admin/model/dto/Franchise/FranchiseUpdateRequest.java
@@ -2,8 +2,12 @@ package com.deliverar.admin.model.dto.Franchise;
 
 
 import com.deliverar.admin.model.dto.Address.AddressRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -12,14 +16,34 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(name = "FranchiseUpdateRequest")
 public class FranchiseUpdateRequest {
 
 
+    @Schema(description = "Franchise ID", example = "1", required = true)
+    @NotBlank(message = "Franchise ID is required")
     private Long id;
+
+    @Schema(description = "Franchise Name", example = "RestaurantExample", required = true)
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Schema(description = "Description of the franchise", example = "Established in ...", required = true)
+    @NotBlank(message = "Desciption is required")
     private String description;
+
+    @Schema(description = "Phone number", example = "4356-3927", required = true)
+    @NotBlank(message = "Phone is required")
     private String phone;
+
+    @Schema(description = "CUIT", example = "20234124327", required = true)
+    @Range(min = 11111111111L, max = 99999999999L, message = "CUIT must have 11 digits")
+    @NotNull(message = "CUIT cannot be NULL")
     private BigInteger cuit;
+
+    @Schema(description = "WebPage of the Franchise", example = "franchiseExample.com")
     private String webPageUrl;
+
+    @Schema(description = "List of Franchise addresses", required = true)
     private List<AddressRequest> addresses;
 }
